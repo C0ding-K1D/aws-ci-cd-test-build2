41580ca0d2d908178b3d8ce6a50a3dffaae9834f
Matthew Alvarez <93284623+C0ding-K1D@users.noreply.github.com>
2023-01-12 17:56:00 -0500

    Update serverApiConfig.js

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -6,8 +6,8 @@ console.log(
 export const API_BASE_URL =
   process.env.NODE_ENV == "production" ||
   process.env.REACT_APP_DEV_REMOTE == "remote"
-    ? "https://starter-mern.herokuapp.com/api/"
-    : "http://Newcloudcrowd-env.eba-tvgugfpd.us-east-1.elasticbeanstalk.com/api/";
+    ? "http://Newcloudcrowd-env.eba-tvgugfpd.us-east-1.elasticbeanstalk.com"
+    : "https://Newcloudcrowd-env.eba-tvgugfpd.us-east-1.elasticbeanstalk.com";
 
 // export const API_BASE_URL = "https://starter-mern.herokuapp.com/api/";
 export const ACCESS_TOKEN_NAME = "x-auth-token";
