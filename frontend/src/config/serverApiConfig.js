ce138b0d7c9b6ea2d349d4881d75eaead7d63423
Matthew Alvarez <93284623+C0ding-K1D@users.noreply.github.com>
2023-01-13 14:48:14 -0500

    Update serverApiConfig.js

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -7,7 +7,7 @@ export const API_BASE_URL =
   process.env.NODE_ENV == "production" ||
   process.env.REACT_APP_DEV_REMOTE == "remote"
     ? "https://172.31.42.16"
-    : "https://172.31.42.16";
+    : "http://localhost:8888";
 
 // export const API_BASE_URL = "https://starter-mern.herokuapp.com/api/";
 export const ACCESS_TOKEN_NAME = "x-auth-token";
