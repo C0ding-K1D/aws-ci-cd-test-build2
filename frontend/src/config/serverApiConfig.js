09e9f772441425d035fb6941dbf338fe2de3bb00
Matthew Alvarez <93284623+C0ding-K1D@users.noreply.github.com>
2023-01-13 14:16:53 -0500

    Update serverApiConfig.js

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -6,7 +6,7 @@ console.log(
 export const API_BASE_URL =
   process.env.NODE_ENV == "production" ||
   process.env.REACT_APP_DEV_REMOTE == "remote"
-    ? "https://Newcloudcrowd-env.eba-tvgugfpd.us-east-1.elasticbeanstalk.com/api"
+    ? "https://Myprojectcicd-env.eba-yztimgpp.us-east-1.elasticbeanstalk.com"
     : "https://Myprojectcicd-env.eba-yztimgpp.us-east-1.elasticbeanstalk.com";
 
 // export const API_BASE_URL = "https://starter-mern.herokuapp.com/api/";
