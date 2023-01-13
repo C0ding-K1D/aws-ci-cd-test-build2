ece640e0637cbd401f3ae0ba0864a50c446ee085
Matthew Alvarez <93284623+C0ding-K1D@users.noreply.github.com>
2023-01-13 14:58:15 -0500

    Update serverApiConfig.js

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -6,8 +6,8 @@ console.log(
 export const API_BASE_URL =
   process.env.NODE_ENV == "production" ||
   process.env.REACT_APP_DEV_REMOTE == "remote"
-    ? "https://Myprojectcicd-env.eba-yztimgpp.us-east-1.elasticbeanstalk.com"
-    : "https://Myprojectcicd-env.eba-yztimgpp.us-east-1.elasticbeanstalk.com";
+    ? "http://127.0.0.1:8888/"
+    : "http://127.0.0.1:8888/";
 
 // export const API_BASE_URL = "https://starter-mern.herokuapp.com/api/";
 export const ACCESS_TOKEN_NAME = "x-auth-token";
